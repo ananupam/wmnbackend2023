19210ac5588dc5acf15f92ca9e6b5ee80fdaf744
Anjali Anupam <2019024@iiitdmj.ac.in>
2023-05-27 15:44:25 +0530

    setup mongoose connection

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const userRouter = require('./routes/userRoutes');
+const mongoose =require('mongoose')
+require("dotenv").config()
+
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req,res,next)=>{
+    console.log("HTTP method - " + req.method + " , URL- " + req.url);
+    next();
+})
+
+
+app.use("/user",userRouter);
+
+app.get('/',(req,res)=>{
+    res.send('Hello, welcome to sign up')
+})
+
+mongoose
+ .connect(process.env.MONGO_URI)
+ .then(()=>{
+    //starting the server and specifying the port, making sure app created after connecting to db
+    app.listen(5004,()=> {
+        console.log('Signup  is running');
+    })
+    console.log('connected to mongoDB')
+ }).catch((error)=>{
+    console.log(error)
+ })
