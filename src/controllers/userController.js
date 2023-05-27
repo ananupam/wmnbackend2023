7c5364d0ca571e54cf3a8c2fbf0dd7eb0d63db67
Anjali Anupam <2019024@iiitdmj.ac.in>
2023-05-27 15:48:54 +0530

    create login controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -32,3 +32,26 @@ const signup = async(req,res) =>{
         res.status(500).json({message:"OOPS! Something went wrong."})
     }
 }
+
+
+const login = async(req,res) =>{
+    const {email,password} = req.body;
+    try{
+        const existingUser = await User.findOne({email : email});
+
+        if(!existingUser){
+            return res.status(400).json({message:"User not found!"});
+        }
+
+        const matchPassword = await  bcrypt.compare(password,existingUser.password);
+        if(!matchPassword){
+            return res.status(400).json({message:"Wrong ID or password!"});
+        }
+
+        const token = jwt.sign({email: existingUser.email, id:existingUser._id},SECRET_KEY);
+        res.status(201).json({user:existingUser, token:token});
+    }catch(error){
+        console.log(error);
+        res.status(500).json({message:"OOPS! Something went wrong."})
+    }
+}
