3a8ebfb385f3ee3c89bd36336bd445c7f04b9b65
Anjali Anupam <2019024@iiitdmj.ac.in>
2023-05-27 19:54:47 +0530

    create doctor controller

---
+++ src/controllers/doctorController.js
@@ -0,0 +1,29 @@
+const Doctor = require("../models/doctorModel");
+
+const getAllDoctors = async(req,res)=>{
+    try{
+        const doctors = await Doctor.find({});
+        res.status(200).json(doctors);
+    }catch(error){
+        console.group(error.message);
+        res.status(500).json({message: error.message});
+    }
+}
+
+const addDoctor = async(req,res)=>{
+    const {name, speciality, experience} = req.body;
+        const newDoctor =new Doctor({
+            name: name,
+            speciality: speciality,
+            experience: experience,
+        });
+    try{
+        await newDoctor.save();
+        res.status(201).json(newDoctor);  
+    }catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message});
+    }
+}
+
+module.exports = {getAllDoctors, addDoctor}
